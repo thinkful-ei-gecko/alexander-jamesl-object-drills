c6065d75b0f04af6057e7941a9e2c931c99b6eda
James Lee <jameswlee109@gmail.com>
2019-08-14 16:31:15 -0500

    drill 6 hard but done

--- objectdrills.js
+++ objectdrills.js
@@ -38,26 +38,26 @@
 //console.log(obj2.meals[3]);
 
 //Arrays of Objects
-let people = [
-  pers1 = {
-    name: 'James',
-    jobTitle: 'Unemployed'
-  },
+// let people = [
+//   pers1 = {
+//     name: 'James',
+//     jobTitle: 'Unemployed'
+//   },
 
-  pers2 = {
-    name: 'Alexander',
-    jobTitle: 'Full time student'
-  },
+//   pers2 = {
+//     name: 'Alexander',
+//     jobTitle: 'Full time student'
+//   },
 
-  pers3 = {
-    name: 'Andrea',
-    jobTitle: 'Instructor'
-  }
-];
+//   pers3 = {
+//     name: 'Andrea',
+//     jobTitle: 'Instructor'
+//   }
+// ];
 
-for(let i=0; i<people.length; i++){
-    console.log(people[i])
-}
+// for(let i=0; i<people.length; i++){
+//     console.log(people[i])
+// }
 
 //Properties that aren't there
 
@@ -67,26 +67,55 @@ for(let i=0; i<people.length; i++){
 //    person.boss = bossName;
 //  }
 
- let people = [
-  pers1 = {
-      name: 'James',
-      jobTitle: 'Unemployed'
-  },
+//  let people = [
+//   pers1 = {
+//       name: 'James',
+//       jobTitle: 'Unemployed'
+//   },
 
-  pers2 = {
-      name: 'Alexander',
-      jobTitle: 'Full time student'
-  },
+//   pers2 = {
+//       name: 'Alexander',
+//       jobTitle: 'Full time student'
+//   },
 
-  pers3 = {
-      name: 'Andrea',
-      jobTitle: 'Instructor'
-  }
-]
-people[0].boss='no one'
-people[1].boss = 'Andrea'
-people[2].boss = 'Thinkful'
+//   pers3 = {
+//       name: 'Andrea',
+//       jobTitle: 'Instructor'
+//   }
+// ]
+// people[0].boss='no one'
+// people[1].boss = 'Andrea'
+// people[2].boss = 'Thinkful'
 
-for(let i=0; i<people.length; i++){
- console.log(`${people[i].jobTitle} ${people[i].name} reports to ${people[i].boss}`)
+// for(let i=0; i<people.length; i++){
+//  console.log(`${people[i].jobTitle} ${people[i].name} reports to ${people[i].boss}`)
+// }
+
+//Cracking the Code
+
+let cipher ={
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4
+  };
+
+function decode(word){
+    for(let ltr in cipher){
+        if(word[0]===ltr){
+            return word[cipher[ltr]]
+        }
+    }
+    return ' '
 }
+
+function decodeWords(sent){
+    let wordArr = sent.split(" ");
+    return wordArr.reduce(function(accumulator, currentValue){
+        return accumulator + decode(currentValue)
+    }, '');
+}
+
+let code = 'craft block argon meter bells brown croon droop'
+console.log(decodeWords(code))
+
