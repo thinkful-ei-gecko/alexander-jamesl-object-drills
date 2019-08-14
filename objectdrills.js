f43e3cdd79fd0c42dfe8e7ab917c9bc62c56c7cc
James Lee <jameswlee109@gmail.com>
2019-08-14 14:19:10 -0500

    object drill 2

---
+++ objectdrills.js
@@ -0,0 +1,22 @@
+//Object initializers and methods
+let loaf = {
+    flour: 300,
+    water: 210,
+    hydration() {
+      return this.water / this.flour * 100;
+    }
+  };
+  
+  console.log(loaf.hydration());
+
+  //Iterating over Obj prop
+  let obj = {
+      foo: 'boo',
+      bar: 'far',
+      fun: 'plum',
+      quux: 'flux',
+      spam: 'pam'
+  }
+
+  for(let keys in obj)
+  console.log(keys)
